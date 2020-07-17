8621690155c8f30bcca7069674af3589e95fd323
R-Sreedhar <63717194+R-Sreedhar@users.noreply.github.com>
2020-07-17 20:06:54 +0530

    Add project files.

---
+++ vsdemopipeline.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace vsdemopipeline.web.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
